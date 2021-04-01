706f7a2c230d3d6409cbb975fe5c944fb392d214
onomatopoetica <72478102+onomatopoetica@users.noreply.github.com>
2021-03-31 21:51:25 -0700

    Updating nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,22 +37,22 @@ function Navbar() {
                 </div>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                     <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                        <Link to='/react-jen-portfolio/' className='nav-links' onClick={closeMobileMenu}>
                             Home
                         </Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                        <Link to='/react-jen-portfolio/services' className='nav-links' onClick={closeMobileMenu}>
                             Services
                         </Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                        <Link to='/react-jen-portfolio/products' className='nav-links' onClick={closeMobileMenu}>
                             Products
                         </Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                        <Link to='/react-jen-portfolio/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
                             Sign Up
                         </Link>
                     </li>
