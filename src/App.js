3c587815a8f58330f698aa223e649248891979ad
onomatopoetica <72478102+onomatopoetica@users.noreply.github.com>
2021-03-31 21:42:17 -0700

    Fixed home path

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/react-portfolio' component={Home} />
+          <Route path='/react-jen-portfolio' component={Home} />
           <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
           <Route path='/sign-up' component={SignUp} />
