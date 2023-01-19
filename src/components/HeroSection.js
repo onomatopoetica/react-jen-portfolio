af24fb4fee8e214e370bdc52f1f6a9c221c6cba7
onomatopoetica <hi.jen@icloud.com>
2023-01-19 08:16:58 -0500

    Fixed mobile margins

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,7 +8,7 @@ function HeroSection() {
   return (
     <div id='home' className='hero-container'>
       <h1>JENNIFER BATES</h1>
-      <p>Technical Project Manager & Software Engineer</p>
+      <p>Project Manager & Software Engineer</p>
       <a href='#projects' className='hero-btns'>
         <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
           CONTACT ME
